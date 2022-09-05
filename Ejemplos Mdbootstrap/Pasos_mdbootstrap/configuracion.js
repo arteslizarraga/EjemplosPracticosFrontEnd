2fe1d063c2d0f3f00c1bfae3aeda2cde0ac4ab42
jmlizarraga <jlizarraga@chilesystems.com>
2022-09-05 17:22:13 -0300

    mdbootsrap pasos

--- Ejemplos Mdbootstrap/Pasos_mdbootstrap/configuracion.js
+++ Ejemplos Mdbootstrap/Pasos_mdbootstrap/configuracion.js
@@ -1,64 +1,39 @@
 
-function pasarSegundoPaso(thisCard) 
+function pasarSegundoPaso() 
 {
     $("#card-paso-2").show();
 
-    if ($(".step1").hasClass("active")) {
-        $(".step1").removeClass("active")
-        $(".step1").addClass("completed");
-        $(".step2").addClass("active");
-    }
-    toggleExpandCard(thisCard, "card-paso-2");
+    $(".step1").removeClass("active");
+    $(".step3").removeClass("active");
+    //$(".step1").addClass("completed");
+    $(".step2").addClass("active");
+
+    $("#card-paso-1").hide();
+    $("#card-paso-3").hide(); 
 }
 
-function pasarTercerPaso(thisCard) 
+function pasarTercerPaso() 
 {
     $("#card-paso-3").show();
 
-    if ($(".step2").hasClass("active")) {
-        $(".step2").removeClass("active");
-        $(".step2").addClass("completed");
-        $(".step3").addClass("active");
-    }
+    $(".step2").removeClass("active");
+    //$(".step2").addClass("completed");
+    $(".step3").addClass("active");
 
-    toggleExpandCard(thisCard, "card-paso-3");
+    $("#card-paso-1").hide();
+    $("#card-paso-2").hide(); 
 }
 
-function volverPaso1(thisCard) 
-{
-    $("#card-paso-1").show();
-
-    if ($(".step2").hasClass("active")) {
-        $(".step2").removeClass("active");
-        $(".step2").removeClass("completed");
-        $(".step1").addClass("active");
-    }
-
-    toggleExpandCard(thisCard, "card-paso-1");
-}
-
-function volverPaso2(thisCard) 
-{
-    $("#card-paso-2").show();
-
-    if ($(".step3").hasClass("active")) {
-        $(".step3").removeClass("active")
-        //$(".step3").addClass("completed");
-        $(".step2").addClass("active");
-    }
-    toggleExpandCard(thisCard, "card-paso-2");
-}
-
-function resetearPasos(thisCard) 
+function resetearPasos() 
 {    
     $("#card-paso-1").show();
 
-    if ($(".step3").hasClass("active")) {
-        $(".step3").removeClass("active");
-        $(".step2").removeClass("completed");
-        $(".step1").addClass("active");
-    }
+    $(".step3").removeClass("active");
+    $(".step2").removeClass("active");
+    $(".step2").removeClass("completed");
+    $(".step1").addClass("active");
 
-    toggleExpandCard(thisCard, "card-paso-1");
+    $("#card-paso-2").hide();
+    $("#card-paso-3").hide();
 }
 
